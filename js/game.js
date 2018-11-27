45c335a498624897d3e633d2b15ab426b931412d
James Van Roose <j.vanroose@nua.ac.uk>
2018-11-27 14:18:41 +0000

    Finished game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,3 @@
-console.log("I am connected");
-
 var config = {
     width: 512,
     height: 512,
@@ -23,13 +21,13 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
-var platforms;
-var player;
+
 var cursors;
-var jewels;
-var score = 0;
-var skulls;
-var scoreText;
+var platforms, jewels, skulls;
+var player;
+var score = 0, scoreText;
+
+//***************** PHASER.SCENE BUILT-IN FUNCTIONS ************//
 
 function preload(){
     console.log(this);
@@ -41,24 +39,47 @@ function preload(){
 }
 
 function create(){
+    cursors = this.input.keyboard.createCursorKeys();
+    
+    createBackground.call(this);
+    createPlatforms.call(this);
+    createPlayer.call(this);
+    createAnimations.call(this);
+    createJewels.call(this);
+    createSkulls.call(this);
+    
+    scoreText = this.add.text(10,10, 'Score: 0', {fontSize: '32px', fill: '#000'});
+    
+    createOverlapAndCollide.call(this);
+}
+
+function update(){
+    checkPlayerMovement();
+}
+
+//***************** NON PHASER.SCENE FUNCTIONS ************//
+//*************** CREATE FUNCTIONS*************************//
+
+function createBackground(){
     var background = this.add.image(config.width/2, config.height/2, "background");    
     background.setScale(2.2, 2.5);
-    
+}
+
+function createPlatforms(){
     platforms = this.physics.add.staticGroup();
     platforms.create(300, 300, "platform");    
-    //platforms.create(100, 300, "platform");    
+    
     var floor = platforms.create(config.width/2, config.height-16, "platform");
     console.log(floor);
     floor.setScale(2.8, 2).refreshBody();
-    
-    
+}
+
+function createPlayer(){
     player = this.physics.add.sprite(100, 100, "player", 1);
     player.setCollideWorldBounds(true);
-    
-    this.physics.add.collider(player, platforms);
-    
-    cursors = this.input.keyboard.createCursorKeys();
-    
+}
+
+function createAnimations(){
     this.anims.create({
         key: "walk",
         frames: this.anims.generateFrameNumbers("player", {start: 4, end: 10}),
@@ -72,7 +93,9 @@ function create(){
         repeat: -1,
         frameRate: 3
     });
-    
+}
+
+function createJewels(){
     jewels = this.physics.add.group({
         key: "jewel",
         setXY: {x: 16, y: 16, stepX: 50},
@@ -80,22 +103,53 @@ function create(){
     });
     
     jewels.children.iterate(function(jewel){
-        jewel.setBounce(Math.random() + 0.3);
+        jewel.setBounce(Math.random());
     });
-    
-    this.physics.add.collider(jewels, platforms);
-    this.physics.add.overlap(player, jewels, pickUpJewel);
-    
-    skulls = this.physics.add.group();
-    this.physics.add.collider(skulls, platforms);
-    createSkull();
-    
-    this.physics.add.overlap(player, skulls, killPlayer);
-    
-    scoreText = this.add.text(10,10, 'Score: 0', {fontSize: '32px', fill: '#000'});
 }
 
-function update(){
+function createSkulls(){
+    skulls = this.physics.add.group();
+    createSkull();
+}
+
+function createSkull(){
+    var skull = skulls.create(Math.random()*config.width, 16, "skull");
+    skull.setCollideWorldBounds(true);
+    skull.setBounce(1);
+    skull.setVelocityX(Math.random()*200);
+}
+
+function createOverlapAndCollide(){
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(jewels, platforms);
+    this.physics.add.overlap(player, jewels, pickUpJewel);
+    this.physics.add.collider(skulls, platforms);
+    this.physics.add.overlap(player, skulls, killPlayer, null, this);
+}
+
+//*************** GAMEPLAY FUNCTIONS *************//
+
+function pickUpJewel(player, jewel){
+    score++;
+    scoreText.setText("Score: " + score);
+    jewel.disableBody(true, true);
+    
+    if(jewels.countActive() === 0){
+        jewels.children.iterate(function(jewel){
+            jewel.enableBody(true, jewel.x, 16, true, true);
+            jewel.setBounce(Math.random());
+        }); 
+        
+        createSkull();
+    }
+}
+
+function killPlayer(){
+    this.physics.pause();
+    player.setTint("#000000");
+}
+
+function checkPlayerMovement(){
     if(cursors.right.isDown){
         player.body.setVelocityX(100);
         player.anims.play("walk", true);
@@ -113,46 +167,3 @@ function update(){
         player.body.setVelocityY(-400);
     }
 }
-
-function pickUpJewel(player, jewel){
-    score++;
-    scoreText.setText("Score: " + score);
-    jewel.disableBody(true, true);
-    
-    if(jewels.countActive() === 0){
-        jewels.children.iterate(function(jewel){
-            jewel.enableBody(true, jewel.x, 16, true, true);
-            jewel.setBounce(Math.random() + 0.3);
-        }); 
-        
-        createSkull();
-    }
-}
-
-function createSkull(){
-    var skull = skulls.create(300, 16, "skull");
-    skull.setCollideWorldBounds(true);
-    skull.setBounce(1);
-    skull.setVelocityX(Math.random()*200);
-}
-
-function killPlayer(){
-    this.physics.pause();
-    player.setTint("#000000");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
